2d138bbf7ad7b017293fa46270fa1cd622d15ea4
authon <authon@vip.qq.com>
2024-01-12 10:26:15 +0800

    Update diy-part.sh

--- build/Lede/diy-part.sh
+++ build/Lede/diy-part.sh
@@ -12,7 +12,7 @@ git clone https://github.com/fw876/helloworld.git -b main package/helloworld
 rm -rf package/passwall
 git clone https://github.com/xiaorouji/openwrt-passwall.git -b main package/passwall
 rm -rf package/passwall2
-git clone https://github.com/xiaorouji/openwrt-passwall2.git -b main package/passwall2
+git clone https://github.com/xiaorouji/openwrt-passwall2.git -b main package/passwall2 &&
 # pushd feeds/packages/lang
 # rm -rf golang && svn co https://github.com/openwrt/packages/branches/openwrt-23.05/lang/golang
 # popd
