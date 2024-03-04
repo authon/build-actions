dcb95aff3710bf199268c9dccfbe70dfde74c468
authon <authon@vip.qq.com>
2024-03-05 07:33:53 +0800

    Update diy-part.sh

--- build/Lede/diy-part.sh
+++ build/Lede/diy-part.sh
@@ -7,6 +7,8 @@
 # 下载额外插件
 # src-git helloworld https://github.com/fw876/helloworld.git;main
 # git clone https://github.com/kenzok8/small.git small
+# sed -i 's/CGO_ENABLED=0/CGO_ENABLED=1/g' /Ledemaster/openwrt/feeds/packages/net/mosdns/Makefile
+# sed -i 's/CGO_ENABLED=0/CGO_ENABLED=1/g' feeds/packages/net/mosdns/Makefile
 # rm -rf package/helloworld
 # git clone https://github.com/fw876/helloworld.git -b main package/helloworld
 # rm -rf package/passwall
@@ -22,12 +24,12 @@
 # popd
 
 # 移除 openwrt feeds 自带的核心包
-rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
-git clone https://github.com/sbwml/openwrt_helloworld package/helloworld
+# rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
+# git clone https://github.com/sbwml/openwrt_helloworld package/helloworld
 
 # 更新 golang 1.22 版本
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
+# rm -rf feeds/packages/lang/golang
+# git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
 
 # 取消默认的 autosamba 依赖的 luci-app-samba 到 slim 里
 find  ./target/linux/ -maxdepth 2 -type f  -name Makefile -exec sed -i 's#zerotier##' {} \;
