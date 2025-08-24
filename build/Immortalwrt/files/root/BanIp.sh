e1583cd7e58759505ea6ba826af038f4f6f1c584
authon <authon@vip.qq.com>
2025-08-25 00:28:26 +0800

    Update BanIp.sh

--- build/Immortalwrt/files/root/BanIp.sh
+++ build/Immortalwrt/files/root/BanIp.sh
@@ -1,115 +1,115 @@
 #!/bin/sh
 #
-# dropBrute.sh by robzr
+# dropBrute.sh by robzr (modified for nftables)
 #
 # minimalist OpenWRT/dropbear ssh brute force attack banning script
+# using nftables instead of iptables
 #
-# Installation steps:
-#
-# 1) Optionally edit the variables in the header of this script to customise
-#    for your environment
-#
-# 2) Insert a reference for this rule in your firewall script before you
-#    accept ssh, something like:
-#
-#    iptables -N dropBrute
-#    iptables -I input_rule -i br-wan -p tcp --dport 22 -j dropBrute
-#    iptables -I input_rule -i br-wan -p tcp --dport 22 -m state --state NEW -m limit --limit 6/min --limit-burst 6 -j ACCEPT
-#
-# 3) Run the script periodically out of cron:
-#
-#    echo '*/10 * * * * /usr/sbin/dropBrute.sh 2>&1 >> /tmp/dropBrute.log' >> /etc/crontabs/root
-#
-# 4) If cron is not enabled, you'll also need to run the following:
-#
-#    /etc/init.d/cron enable && /etc/init.d/cron start
-#
-#
-# To whitelist hosts or networks, simply add a manual entry to the lease
-# file with a leasetime of -1.  This can be done with the following syntax:
-#
-#    echo -1 192.168.1.0/24 >> /tmp/dropBrute.leases
-#
-# A static, or non-expiring blacklist of a host or network can also be
-# added, just use a lease time of 0.  This can be done with the following syntax:
-#
-#    echo 0 1.2.3.0/24 >> /tmp/dropBrute.leases
 
-# How many bad attempts before banning. Only the log entries from the
-# current day are checked.
+# How many bad attempts before banning (based on recent attempts)
 allowedAttempts=10
 
-# How long IPs are banned for after the current day ends.
-# default is 7 days
-secondsToBan=$((36500*60*60*24))
+# How long IPs are banned for (7 days in seconds)
+secondsToBan=$((60*60*24*7))
 
-# the "lease" file - defaults to /tmp which does not persist across reboots
-leaseFile=/tmp/dropBrute.leases
+# the "lease" file - stored in /etc to persist across reboots
+leaseFile=/etc/dropBrute.leases
 
-# This is the iptables chain that drop commands will go into.
-# you will need to put a reference in your firewall rules for this
-iptChain=dropBrute
+# This is the nftables chain that drop commands will go into.
+nftChain=dropBrute
 
-# the IP Tables drop rule
-iptDropRule='-j DROP'
-
-# the IP Tables whitelist rule
-iptWhiteRule='-j RETURN'
+# nftables table and family
+nftTable=inet filter
+nftFamily=inet
 
 # You can put default leasefile entries in the following space.
 # Syntax is simply "leasetime _space_ IP_or_network".  A leasetime of -1 is a
 # whitelist entry, and a leastime of 0 is a permanent blacklist entry.
 [ -f $leaseFile ] || cat <<__EOF__>>$leaseFile
--1 192.168.1.0/24
+-1 10.10.10.1/24
 __EOF__
 
-# End of user customizable variables (unless you know better :) )
+# End of user customizable variables
 
-ipt='/usr/sbin/iptables'
+nft='/usr/sbin/nft'
 
-[ `date +'%s'` -lt 1320000000 ] && echo System date not set, aborting. && exit -1
-$ipt -N $iptChain >&/dev/null
+# Check if system date is set correctly
+[ `date +'%s'` -lt 1609459200 ] && echo "System date not set correctly, aborting." && exit -1
+
+# Initialize nftables configuration if needed
+$nft list table $nftFamily $nftTable >/dev/null 2>&1 || {
+  echo "Creating nftables table $nftTable"
+  $nft add table $nftFamily $nftTable
+}
+
+# Create chain if it doesn't exist
+$nft list chain $nftTable $nftChain >/dev/null 2>&1 || {
+  echo "Creating nftables chain $nftChain"
+  $nft add chain $nftTable $nftChain "{ type filter hook input priority 0; policy accept; }"
+  # Add reference to our chain in the input path for SSH
+  $nft add rule $nftTable input tcp dport 22 jump $nftChain
+  # Add rate limiting for new connections
+  $nft add rule $nftTable input tcp dport 22 ct state new limit rate 6/minute burst 6 packets accept
+}
 
 today=`date +'%b %d'`
 now=`date +'%s'`
 nowPlus=$((now + secondsToBan))
 
-echo Running dropBrute on `date` \($now\)
+echo "Running dropBrute on `date` ($now)"
 
-if [ `$ipt -S $iptChain|fgrep "$iptChain"|wc -l` -lt 1 ] ; then
-  $ipt -N $iptChain
-fi
-if [ `$ipt -S input_wan_rule|fgrep "$iptChain"|wc -l` -lt 1 ] ; then
-  $ipt -I input_wan_rule -p tcp --dport 22 -j $iptChain
-fi
-
-# find new badIPs
-for badIP in `logread|awk -F 'from |:' /dropbear.*attempt.*from/'{print $(NF-1)}'|sort -u` ; do
-  found=`logread|awk -F 'from |:' /dropbear.*attempt.*from/'{print $(NF-1)}'|fgrep $badIP|wc -l`
-  if [ $found -gt $allowedAttempts ] ; then
-    if [ `egrep \ $badIP\$ $leaseFile|wc -l` -gt 0 ] ; then
-       [ `egrep \ $badIP\$ $leaseFile|cut -f1 -d\ ` -gt 0 ] && sed -i 's/^.* '$badIP\$/$nowPlus\ $badIP\/ $leaseFile
+# Find new bad IPs - only check today's logs
+for badIP in `logread | grep "$today" | awk -F 'from |:' '/dropbear.*attempt.*from/ {print $(NF-1)}' | sort -u`; do
+  # Count attempts from this IP today
+  found=`logread | grep "$today" | awk -F 'from |:' '/dropbear.*attempt.*from/ {print $(NF-1)}' | fgrep -c $badIP`
+  
+  if [ $found -gt $allowedAttempts ]; then
+    # Check if IP is already in lease file
+    if grep -q " $badIP$" $leaseFile; then
+      # Update expiration time if it's a temporary ban
+      currentLease=$(grep " $badIP$" $leaseFile | cut -f1 -d' ')
+      if [ $currentLease -gt 0 ]; then
+        sed -i "s/^.* $badIP$/$nowPlus $badIP/" $leaseFile
+        echo "Updated ban expiration for $badIP until $(date -d @$nowPlus)"
+      fi
     else
-       echo $nowPlus $badIP >> $leaseFile
+      # Add new temporary ban
+      echo "$nowPlus $badIP" >> $leaseFile
+      echo "Added new ban for $badIP until $(date -d @$nowPlus)"
     fi
   fi
 done
 
-# now parse the leaseFile
-while read lease ; do
-  leaseTime=`echo $lease|cut -f1 -d\ `
-  leaseIP=`echo $lease|cut -f2 -d\ `
-  if [ $leaseTime -lt 0 ] ; then
-    if [ `$ipt -S $iptChain|egrep \ $leaseIP/32\ \|\ $leaseIP\ |fgrep -- "$iptWhiteRule"| wc -l` -lt 1 ] ; then
-      echo Adding new whitelist rule for $leaseIP
-      $ipt -I $iptChain -s $leaseIP $iptWhiteRule
+# Process lease file and update nftables rules
+while read -r leaseTime leaseIP; do
+  # Skip empty lines
+  [ -z "$leaseTime" ] || [ -z "$leaseIP" ] && continue
+  
+  # Check if rule for this IP already exists
+  ruleExists=$($nft list ruleset | grep -c "$nftTable $nftChain .* $leaseIP")
+  
+  if [ $leaseTime -lt 0 ]; then
+    # Whitelist rule (-1 = permanent whitelist)
+    if [ $ruleExists -eq 0 ]; then
+      echo "Adding whitelist rule for $leaseIP"
+      $nft add rule $nftTable $nftChain ip saddr $leaseIP accept
     fi
-  elif [ $leaseTime -ge 1 -a $now -gt $leaseTime ] ; then
-    echo Expiring lease for $leaseIP
-    $ipt -D $iptChain -s $leaseIP $iptDropRule
-    sed -i /$leaseIP/d $leaseFile
-  elif [ $leaseTime -ge 0 -a `$ipt -S $iptChain|egrep \ $leaseIP/32\ \|\ $leaseIP\ |wc -l` -lt 1 ] ; then
-    echo Adding new rule for $leaseIP
-    $ipt -A $iptChain -s $leaseIP $iptDropRule
+  elif [ $leaseTime -eq 0 ]; then
+    # Permanent blacklist (0 = permanent blacklist)
+    if [ $ruleExists -eq 0 ]; then
+      echo "Adding permanent blacklist rule for $leaseIP"
+      $nft add rule $nftTable $nftChain ip saddr $leaseIP drop
+    fi
+  elif [ $now -gt $leaseTime ]; then
+    # Expired temporary ban - remove it
+    echo "Removing expired ban for $leaseIP"
+    $nft delete rule $nftTable $nftChain ip saddr $leaseIP drop 2>/dev/null
+    sed -i "/ $leaseIP$/d" $leaseFile
+  else
+    # Active temporary ban
+    if [ $ruleExists -eq 0 ]; then
+      echo "Adding temporary ban for $leaseIP until $(date -d @$leaseTime)"
+      $nft add rule $nftTable $nftChain ip saddr $leaseIP drop
+    fi
   fi
 done < $leaseFile
